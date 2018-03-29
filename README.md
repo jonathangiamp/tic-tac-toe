12155f4906809311282eedec5faa10f3970a4185
jonathangiamp <jonathan.giamporcaro@yobot.xyz>
2018-03-29 13:28:05 +0200

    use gridcss for square

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **ToDo :**
 + ~~Add .eslintrc~~
 + ~~Complete package.json~~
-+ Use grid for square
++ ~~Use grid for square~~
 + Show players' numbers when it's their turns
 + Develop and display a score system
 + Make the game responsive
--- src/components/Board.js
+++ src/components/Board.js
@@ -14,21 +14,15 @@ class Board extends Component {
   render() {
     return (
       <div className="container-board-row">
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
       </div>
     );
   }
